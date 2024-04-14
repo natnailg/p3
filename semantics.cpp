98ae8c3933fa24019f31f33e3f6e9042a5ef4475
natnailg <natnailgebru@gmail.com>
2024-04-13 21:04:59 -0500

    working printing. and exiting,

--- semantics.cpp
+++ semantics.cpp
@@ -77,9 +77,9 @@ bool Table_check(node_t* identifier){
         //strcmp(first_str, second_str );
         result = strcmp(table_array[i], identifier->token_instance);  // this means that it found it in the table
         if (result == 0) { // if in the table
-            printf("Error: It is already declared in the table %s\n", root->token_instance);
+            printf("Error: It is already declared in the table %s\n", table_array[i]);
             exit(EXIT_FAILURE);
-         //printf("Already in the table table %s\n", table_array[i]);
+            //printf("Already in the table table %s\n", table_array[i]);
             string_check = true;
             return string_check;
         }
