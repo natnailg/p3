c1101b3fdab53b5c5cf84a8115bc7d8849831911
natnailg <natnailgebru@gmail.com>
2024-04-13 21:21:37 -0500

    Every thing seems to be working, test it tomorrow and turn it in.

--- semantics.cpp
+++ semantics.cpp
@@ -81,8 +81,8 @@ bool Table_check(node_t* identifier){
             printf("Error: It is already declared in the table %s\n", table_array[i]);
             exit(EXIT_FAILURE);
             //printf("Already in the table table %s\n", table_array[i]);
-//            string_check = true;
-//            return string_check;
+            // string_check = true;
+            //  return string_check;
         }
     }
 
